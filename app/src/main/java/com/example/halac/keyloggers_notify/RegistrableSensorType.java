ef9c0a62792a60c85765079b47c04ae980017c90
ZoulfikarShmayssani <zas23@mail.aub.edu>
2018-10-03 21:40:56 +0300

    Added the missing enum types

--- app/src/main/java/com/example/halac/keyloggers_notify/RegistrableSensorType.java
+++ app/src/main/java/com/example/halac/keyloggers_notify/RegistrableSensorType.java
@@ -6,7 +6,11 @@ public enum RegistrableSensorType {
     humidity(0, Sensor.TYPE_RELATIVE_HUMIDITY),
     accelerometer(1, Sensor.TYPE_ACCELEROMETER),
     stepCounter(2, Sensor.TYPE_STEP_COUNTER),
-    temperature(3, Sensor.TYPE_AMBIENT_TEMPERATURE);
+    temperature(3, Sensor.TYPE_AMBIENT_TEMPERATURE),
+    light(4, Sensor.TYPE_LIGHT),
+    linearAcceleration(5, Sensor.TYPE_LINEAR_ACCELERATION),
+    gyroscope(6, Sensor.TYPE_GYROSCOPE),
+    proximity(7, Sensor.TYPE_PROXIMITY);
 
     public final int index;
     public final int sensorId;
